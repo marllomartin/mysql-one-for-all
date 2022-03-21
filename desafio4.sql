ed6136980f4edaca30386b52d31af1594f5c2898
Marllon <marllon3636@gmail.com>
2022-03-21 03:01:00 -0300

    feat(root/desafio4.sql): Cria comando do Req.4

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+SELECT
+    users.user_name AS usuario,
+    IF(YEAR(MAX(history.history_date)) >= '2021', 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+
+FROM
+    SpotifyClone.users
+    JOIN SpotifyClone.history ON history.user_id = users.user_id
+
+GROUP BY usuario;
