2bc72ce1874fdb9efeb8e20c58520a746ce55a09
Marllon <marllon3636@gmail.com>
2022-03-21 03:00:48 -0300

    feat(root/desafio3.sql): Cria comando do Req.3

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+SELECT
+  users.user_name AS usuario,
+  COUNT(history.song_id) AS qtde_musicas_ouvidas,
+  ROUND(SUM(songs.song_duration) / 60, 2) AS total_minutos
+
+FROM SpotifyClone.users
+  INNER JOIN SpotifyClone.history ON history.user_id = users.user_id
+  INNER JOIN SpotifyClone.songs ON songs.song_id = history.song_id
+
+GROUP BY users.user_name ORDER BY users.user_name;
